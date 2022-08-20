f89ba6f863b80780d1734c369a3052c412a5799b
kiselmen <kiselmen@rambler.ru>
2022-08-20 17:45:10 +0300

    refactor: move login functions from form lo login page

--- src/components/forms/login.ts
+++ src/components/forms/login.ts
@@ -1,16 +1,8 @@
 import { Component } from "../../utils/component";
-import { load } from "../../utils/loader";
 import { UIInput } from "../UI/input";
 import { UIButton } from "../UI/button";
 
-const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-// const BASE_URL = "http://localhost:8081";
-
 import "./login.scss";
-
-export type elementData = {
-  [key: string] : string 
-}
 
 export class LoginForm extends Component {
   eMailInput: UIInput;
@@ -36,97 +28,5 @@ export class LoginForm extends Component {
     const buttonContainer = new Component(this.element, "div", ["form-item", "item"]);
     this.buttonLogin = new UIButton(buttonContainer.element, ["btn"], "Login");
     this.buttonRegister = new UIButton(buttonContainer.element, ["btn"], "Register");
-
-    this.buttonLogin.onClickButton = async (e: Event) => this.onLogin(e);
-    this.buttonRegister.onClickButton = async (e: Event) => this.onRegister(e);
   }
-
-  onLogin = async (e: Event) => {
-    e.preventDefault();
-    const eMailElement = this.eMailInput.element as HTMLInputElement;
-    const passElement = this.passInput.element as HTMLInputElement;
-
-    const isValidate = this.onValidateEmail(eMailElement) && this.onValidatePass(passElement);
-
-    console.log(isValidate);
-
-  };
-
-  onRegister = async (e: Event) => {
-    e.preventDefault();
-    const eMailElement = this.eMailInput.element as HTMLInputElement;
-    const passElement = this.passInput.element as HTMLInputElement;
-
-    const isValidate = this.onValidateEmail(eMailElement) && this.onValidatePass(passElement);
-    if (isValidate) {
-      const userData = this.onDataConstruct(eMailElement, passElement);
-      
-      const response = await this.onRegisterUser(userData);
-      // console.log(response);
-      // 6300d094b6e08c4e204166b5
-      if (response.status !== 200) {
-        // console.log("Error ---", response);
-        eMailElement.value = "";
-        eMailElement.placeholder = "This email is in use by another user";
-      } else {
-        const response = await this.onSignInUser(userData);
-        if (response.status !== 200) {
-          passElement.value = "";
-          passElement.placeholder = "Wrong password or EMail";
-        } else {
-          localStorage.setItem("token", response.data.token);
-        }
-      }
-    }
-  };
-  
-  onValidateEmail: (elem: HTMLInputElement) => boolean = (elem) => {
-    const isEmailValidate = EMAIL_REGEXP.test(elem.value);
-    if (!isEmailValidate) {
-      elem.value = "";
-      elem.placeholder = "Invalid EMail";
-    }
-    return isEmailValidate;
-  };
-
-  onValidatePass: (elem: HTMLInputElement) => boolean = (elem) => {
-    const isPassValidate = elem.value.length > 7;
-    if (!isPassValidate) {
-      elem.value = "";
-      elem.placeholder = "Password length must be more then 7";
-    }
-    return isPassValidate;
-  };
-  
-  onDataConstruct: (eMail: HTMLInputElement, pass: HTMLInputElement) => elementData = (eMail, pass) => {
-    const userData: elementData = {};
-    userData.email = eMail.value;
-    userData.password = pass.value;
-    return userData;
-  };
-
-  async onRegisterUser(userData: elementData) {
-    const url = "/users";
-    const method = {
-      method : "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData)
-    };
-    return await load(url, method);     
-  }
-
-  async onSignInUser(userData: elementData) {
-    const url = "/signin";
-    const method = {
-      method : "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData)
-    };
-    return await load(url, method);     
-  }
-}
-
+}  
--- src/pages/login.ts
+++ src/pages/login.ts
@@ -1,5 +1,9 @@
 import { Component } from "../utils/component";
 import { LoginForm } from "../components/forms/login";
+import { load } from "../utils/loader";
+import { elementData } from "../interfaces";
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 
 export class Login extends Component{
   loginForm : LoginForm;
@@ -7,5 +11,104 @@ export class Login extends Component{
   constructor(parentNode: HTMLElement) {
     super(parentNode, "div", ["container"], "");
     this.loginForm = new LoginForm(this.element);
+
+    this.loginForm.buttonLogin.onClickButton = async (e: Event) => this.onLogin(e);
+    this.loginForm.buttonRegister.onClickButton = async (e: Event) => this.onRegister(e);
+  }
+
+  onLogin = async (e: Event) => {
+    e.preventDefault();
+    const eMailElement = this.loginForm.eMailInput.element as HTMLInputElement;
+    const passElement = this.loginForm.passInput.element as HTMLInputElement;
+
+    const isValidate = this.onValidateEmail(eMailElement) && this.onValidatePass(passElement);
+
+    if (isValidate) {
+      const userData = this.onDataConstruct(eMailElement, passElement);
+      const response = await this.onSignInUser(userData);
+      if (response.status !== 200) {
+        passElement.value = "";
+        passElement.placeholder = "Wrong password or EMail";
+      } else {
+        localStorage.setItem("token", response.data.token);
+      }
+    }  
+  };
+
+  onRegister = async (e: Event) => {
+    e.preventDefault();
+    const eMailElement = this.loginForm.eMailInput.element as HTMLInputElement;
+    const passElement = this.loginForm.passInput.element as HTMLInputElement;
+
+    const isValidate = this.onValidateEmail(eMailElement) && this.onValidatePass(passElement);
+    if (isValidate) {
+      const userData = this.onDataConstruct(eMailElement, passElement);
+      
+      const response = await this.onRegisterUser(userData);
+      // console.log(response);
+      // 6300d094b6e08c4e204166b5
+      if (response.status !== 200) {
+        // console.log("Error ---", response);
+        eMailElement.value = "";
+        eMailElement.placeholder = "This email is in use by another user";
+      } else {
+        const response = await this.onSignInUser(userData);
+        if (response.status !== 200) {
+          passElement.value = "";
+          passElement.placeholder = "Wrong password or EMail";
+        } else {
+          localStorage.setItem("token", response.data.token);
+        }
+      }
+    }
+  };
+  
+  onValidateEmail: (elem: HTMLInputElement) => boolean = (elem) => {
+    const isEmailValidate = EMAIL_REGEXP.test(elem.value);
+    if (!isEmailValidate) {
+      elem.value = "";
+      elem.placeholder = "Invalid EMail";
+    }
+    return isEmailValidate;
+  };
+
+  onValidatePass: (elem: HTMLInputElement) => boolean = (elem) => {
+    const isPassValidate = elem.value.length > 7;
+    if (!isPassValidate) {
+      elem.value = "";
+      elem.placeholder = "Password length must be more then 7";
+    }
+    return isPassValidate;
+  };
+  
+  onDataConstruct: (eMail: HTMLInputElement, pass: HTMLInputElement) => elementData = (eMail, pass) => {
+    const userData: elementData = {};
+    userData.email = eMail.value;
+    userData.password = pass.value;
+    return userData;
+  };
+
+  async onRegisterUser(userData: elementData) {
+    const url = "/users";
+    const method = {
+      method : "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData)
+    };
+    return await load(url, method);     
+  }
+
+  async onSignInUser(userData: elementData) {
+    const url = "/signin";
+    const method = {
+      method : "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData)
+    };
+    return await load(url, method);     
   }
 }
