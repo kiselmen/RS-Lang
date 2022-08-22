f83ff7c0456d7db12e60d02300eabd9dbd0544c3
pitbrest <sasha.petrov@tut.by>
2022-08-22 16:40:34 +0300

    feat: start working whis about page

--- src/pages/about.ts
+++ src/pages/about.ts
@@ -1,7 +1,17 @@
 import { Component } from "../utils/component";
+import "./about.scss";
 
 export class About extends Component {
-  constructor(parentNode: HTMLElement) {
+  private controlsWrap;
+  private aboutProgectBtn;
+  private aboutPTeamBtn;
+
+  constructor(parentNode: HTMLElement) {    
     super(parentNode, "div", ["about"]);
+
+    this.controlsWrap = new Component(this.element, "div", ["aboutControls-wrap"]);
+    this.aboutProgectBtn = new Component(this.controlsWrap.element, "button", ["aboutProgect-btn"], "About project");
+    this.aboutPTeamBtn = new Component(this.controlsWrap.element, "button", ["aboutProgect-btn"], "About team");
+
   }
 }
