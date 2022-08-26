d34c0117ab41feeef009b6989aef3e0ad6f083a8
maxomeleneckii <max.omeleneckii@gmail.com>
2022-08-26 11:07:07 +0300

    feat: create home-page for audiocall game; add styles for home-page. refactor: add mixins in styles and refactor some files.

---
+++ src/components/audio-call/start-page.ts
@@ -0,0 +1,32 @@
+import { Component } from "../../utils/component";
+import { UIButton } from "../UI/button";
+import "./start-page.scss";
+
+export class AudioCallStartPage extends Component {
+  private homeHeading: Component;
+  private homeContent: Component;
+  private homeDescription: Component;
+  private homeBtnsChoose: Component;
+  private homeLvl: Component;
+  private homeLvlBtnsWrapper: Component;
+  homeLvlBtns: UIButton | undefined;
+  private linkMainPage: Component;
+
+  constructor(parentNode: HTMLElement) {
+    super(parentNode, "div", ["audiocall-home", "home"]);
+
+    this.homeHeading = new Component(this.element, "h2", ["home-heading"], "Audio Сall Game");
+    this.homeContent = new Component(this.element, "div", ["home-content"]);
+    this.homeDescription = new Component(this.homeContent.element, "p", ["home-content__descript"], "It will help to understand the pronunciation of sounds, words and connectives in phrases. Improves listening comprehension.");
+    this.homeLvl = new Component(this.homeContent.element, "div", ["home-lvl"]);
+    this.homeBtnsChoose = new Component(this.homeLvl.element, "span", ["home-lvl__span"], "Choose a level:");
+    this.homeLvlBtnsWrapper = new Component(this.homeLvl.element, "div", ["home-lvl__btns"]);
+    for(let i = 0; i < 6; i+=1) {
+      this.homeLvlBtns = new UIButton(this.homeLvlBtnsWrapper.element, ["btn", "home-btn"], `${i+1}`);
+    }
+    this.linkMainPage = new Component(this.homeContent.element, "a", ["btn", "home-link__back"], "Back to main page"
+    );
+
+    this.linkMainPage.element.setAttribute("href", "#/");
+  }
+}
--- src/pages/audiocall.ts
+++ src/pages/audiocall.ts
@@ -1,7 +1,12 @@
 import { Component } from "../utils/component";
+import { AudioCallStartPage } from "../components/audio-call/start-page";
 
 export class AudioCall extends Component {
+  private homePage: AudioCallStartPage;
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, "div", ["audiocall"]);
+
+    this.homePage = new AudioCallStartPage(this.element);
   }
 }
