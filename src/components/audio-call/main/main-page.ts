db6d60fbb429b923ff89f0b1e31c6df34da83243
maxomeleneckii <max.omeleneckii@gmail.com>
2022-08-29 03:06:06 +0300

    feat: add statistic page; add styles for statistic; add functionality for audiocall. refactor: change progressbar place.

--- src/components/audio-call/main/main-page.ts
+++ src/components/audio-call/main/main-page.ts
@@ -1,10 +1,14 @@
 import { Component } from "../../../utils/component";
-import ProgressBar from "progressbar.js";
 import Circle from "progressbar.js/circle";
 import "./main-page.scss";
 import { UIButton } from "../../UI/button";
 import { getWordsByChapterAndPage } from "../../../utils/loader";
 import { BASE_URL, IWordsElement } from "../../../interfaces";
+import { progressBarMixin } from "../progressBar";
+
+
+export let correctWords: IWordsElement[] = [];
+export let wrongWords: IWordsElement[] = [];
 
 export class AudioCallMainPage extends Component {
   private audioCallHeader: Component;
@@ -20,11 +24,15 @@ export class AudioCallMainPage extends Component {
   private actionBtn: UIButton;
   private audioCallProgressbar: Component;
   private progressbarCount: Component;
+  private progressVal = 0;
   private listWords: Component;
-  listBtn: UIButton | undefined;
-  listItem: Component | undefined;
+  private textDontKnow: Component;
+  private textNext: Component;
+  listBtn!: UIButton;
+  listItem!: Component;
   private bar: Circle;
   valChapter = 0;
+  valPage = 0;
   arrWords: IWordsElement[] = [];
   randomNum!: IWordsElement;
 
@@ -44,35 +52,28 @@ export class AudioCallMainPage extends Component {
     this.audio = new Component(this.voiceWrapper.element, "audio", [],);
     this.audioResult = new Component(this.voiceWrapper.element, "audio", [],);
     this.listWords = new Component(this.audioCallContent.element, "ul", ["master-words", "words"]);
-    this.actionBtn = new UIButton(this.audioCallContent.element, ["master-action__btn", "btn"], "I don't know");
+    for(let i = 0; i < 5; i+=1) {
+      this.listItem = new Component(this.listWords.element, "li", ["words-item"],);
+      this.listBtn = new UIButton(this.listItem.element, ["words-btn", "btn"], "");
+    }
+    this.actionBtn = new UIButton(this.audioCallContent.element, ["master-action__btn", "btn"], "");
+    this.textDontKnow = new Component(this.actionBtn.element, "span", ["action-item"], "I don't know");
+    this.textNext = new Component(this.actionBtn.element, "span", ["action-item", "next"], "Next");
     
     this.audioCallProgressbar.element.setAttribute("id", "container");
 
-    this.bar = new ProgressBar.Circle(this.audioCallProgressbar.element, {
-      strokeWidth: 6,
-      easing: "easeInOut",
-      duration: 1400,
-      color: "#ff9f00",
-      trailColor: "#f6e369",
-      trailWidth: 4,
-      svgStyle: null,
-      step: function(state, circle) {
-        const value = Math.round(circle.value() * 100);
-        if (value === 0) {
-          (<HTMLElement>document.querySelector(".master-progressbar__count")).innerHTML = "0%";
-        } else {
-          (<HTMLElement>document.querySelector(".master-progressbar__count")).innerHTML = `${value}%`;
-        }
-      }
-    });
+    this.bar = progressBarMixin(this.audioCallProgressbar.element);
 
     this.controlsClose.element.addEventListener("click", () => {
+      this.progressVal = 0;
+      correctWords = [];
+      wrongWords = [];
+      this.bar.animate(0);
       (<HTMLElement>document.querySelector(".master")).style.display = "none";
       (<HTMLElement>document.querySelector(".home")).style.display = "flex";
     });
-
+    
     (<HTMLElement>document.querySelector(".home-lvl__btns")).addEventListener("click", (e) => {
-      this.listWords.element.innerHTML = "";
       const eventTarget = e.target as HTMLElement;
       const { id } = eventTarget;
       this.valChapter = +id;
@@ -80,25 +81,42 @@ export class AudioCallMainPage extends Component {
       if (eventTarget.classList.contains("home-btn")) {
         (<HTMLElement>document.querySelector(".master")).style.display = "flex";
         (<HTMLElement>document.querySelector(".home")).style.display = "none";
-        this.getWords(this.valChapter);
+        this.textDontKnow.element.style.display = "flex";
+        this.textNext.element.style.display = "none";
+        this.getWords(this.valChapter, this.valPage);
       }
     });
     
     this.voiceBtn.element.addEventListener("click", () => {
       (<HTMLAudioElement>this.audio.element).play();
     });
+
+    this.textDontKnow.element.addEventListener("click", () => {
+      this.getCorrectWord();
+      wrongWords.push(this.randomNum);
+    });
+
+    this.textNext.element.addEventListener("click", () => this.getWords(this.valChapter, this.valPage +=1));
     
-    this.listWords.element.addEventListener("click", this.guessWord, {once: true});
-    
-    this.bar.animate(0.0);
+    this.listWords.element.addEventListener("click", this.guessWord);
   }
-  getWords = (numChapter: number) => {
-    getWordsByChapterAndPage(numChapter,0).then( data => {
+
+  getWords = (numChapter: number, numPage: number) => {
+    document.querySelectorAll(".words-btn").forEach( el => {
+      el.innerHTML = "";
+      el.removeAttribute("disabled");
+      (<HTMLElement>el).style.background = "transparent";
+    });
+    this.voiceWrapper.element.style.background = `none`;
+    this.voiceBtn.element.classList.remove("showPicture");
+    this.textDontKnow.element.style.display = "flex";
+    this.textNext.element.style.display = "none";
+    getWordsByChapterAndPage(numChapter, numPage).then( data => {
       this.arrWords = data.map( (dataItm: IWordsElement, idx: number) => {
         if(idx < 5) {
-          this.listItem = new Component(this.listWords.element, "li", ["words-item"],);
-          this.listBtn = new UIButton(this.listItem.element, ["words-btn", "btn"], `${idx + 1}. ${dataItm.wordTranslate}`);
-          this.listBtn.element.setAttribute("id", dataItm.word);
+          const wordBtn = (<HTMLElement>document.querySelectorAll(".words-btn")[idx]);
+          wordBtn.innerHTML = `${idx + 1}. ${dataItm.wordTranslate}`;
+          wordBtn.setAttribute("id", dataItm.word);
           return dataItm;
         }
       }).filter( (dataItm: IWordsElement) => dataItm !== undefined);
@@ -123,11 +141,29 @@ export class AudioCallMainPage extends Component {
       if(this.randomNum.word === id) {
         eventTarget.style.background = "#67db67";
         this.audioResult.element.setAttribute("src", "../../../../public/audio/win.mp3");
+        this.bar.animate(this.progressVal += 0.1);
+        localStorage.setItem("progressbarVal", `${this.progressVal}`);
+        correctWords.push(this.randomNum);
       } else {
         eventTarget.style.background = "#ff4c4c";
         this.audioResult.element.setAttribute("src", "../../../../public/audio/wrong.mp3");
+        wrongWords.push(this.arrWords.filter( el => el.word === id)[0]);
       }
       (<HTMLAudioElement>this.audioResult.element).play();
+      this.getCorrectWord();
     }
+  };
+  
+  getCorrectWord = () => {
+    document.querySelectorAll(".words-btn").forEach( el => {
+      if (el.id === this.randomNum.word) {
+        (<HTMLElement>el).style.background = "#67db67";
+        this.voiceWrapper.element.style.background = `url(${BASE_URL + this.randomNum.image}) center / contain no-repeat`;
+        this.voiceBtn.element.classList.add("showPicture");
+      }
+      el.setAttribute("disabled", "true");
+      this.textDontKnow.element.style.display = "none";
+      this.textNext.element.style.display = "flex";
+    });
   };
 }
---
+++ src/components/audio-call/statistic/stat-page.ts
@@ -0,0 +1,79 @@
+import Circle from "progressbar.js/circle";
+import { IWordsElement } from "../../../interfaces";
+import { Component } from "../../../utils/component";
+import { UIButton } from "../../UI/button";
+import { correctWords, wrongWords } from "../main/main-page";
+import { progressBarMixin } from "../progressBar";
+import "./stat-page.scss";
+
+export class AudioCallStatisticPage extends Component {
+  private statHeading: Component;
+  private statProgressbar: Component;
+  private progressbarCount: Component;
+  private statTotalCont: Component;
+  private statBtnsCont: Component;
+  private totalCorrect: Component;
+  private totalWrong: Component;
+  private correctCont: Component;
+  private wrongCont: Component;
+  private correctHeading: Component;
+  private wrongHeading: Component;
+  private btnAgain: UIButton;
+  private btnDictionary: UIButton;
+  private linkDictionary: Component;
+  private linkAgain: Component;
+  animateVal = localStorage.getItem("progressbarVal") as string;
+  totalItemHyphen!: Component;
+  totalCorrectItem!: Component;
+  totalWrongItem!: Component;
+  totalItemImg!: Component;
+  totalItemEnWord!: Component;
+  totalItemRuWord!: Component;
+  totalItemBtn!: UIButton;
+  bar: Circle;
+  totalItem!: Component;
+
+  constructor(parentNode: HTMLElement) {
+    super(parentNode, "div", ["audiocall-stat", "statistic"]);
+
+    this.statHeading = new Component(this.element, "h3", ["statistic-heading"],"Here is your result");
+    this.statProgressbar = new Component(this.element, "div", ["master-progressbar", "statistic-progressbar"]);
+    this.statTotalCont = new Component(this.element, "div", ["statistic-total", "total"]);
+    this.correctCont = new Component(this.statTotalCont.element, "div", ["total-correct__cont"]);
+    this.wrongCont = new Component(this.statTotalCont.element, "div", ["total-wrong__cont"]);
+    this.totalCorrect = new Component(this.correctCont.element, "ul", ["total-list", "total-correct"]);
+    this.correctHeading = new Component(this.totalCorrect.element, "h4", ["total-heading"], "Correct:");
+    this.totalWrong = new Component(this.wrongCont.element, "ul", ["total-list", "total-wrong"]);
+    this.wrongHeading = new Component(this.totalWrong.element, "h4", ["total-heading"], "Wrong:");
+    this.statBtnsCont = new Component(this.element, "div", ["statistic-btns"]);
+    this.btnAgain = new UIButton(this.statBtnsCont.element, ["statistic-btns__again"], "Again");
+    this.linkAgain = new Component(this.btnAgain.element, "a", ["statistic-link"], "");
+    this.btnDictionary = new UIButton(this.statBtnsCont.element, ["statistic-btns__dictionary"], "Dictionary");
+    this.linkDictionary = new Component(this.btnDictionary.element, "a", ["statistic-link"], "");
+
+    this.linkAgain.element.setAttribute("href", "#/audiocall");
+    this.linkDictionary.element.setAttribute("href", "#/dictionary");
+    
+    this.progressbarCount = new Component(this.statProgressbar.element, "span", ["master-progressbar__count"],);
+    this.bar = progressBarMixin(this.statProgressbar.element);
+
+    if (correctWords.length + wrongWords.length === 10) {
+      this.bar.animate(+this.animateVal);
+      this.createList(this.totalCorrect.element, correctWords);
+      this.createList(this.totalWrong.element, wrongWords);
+    }
+  }
+  
+  createList = (listContainer: HTMLElement, arrayWords: IWordsElement[]) => {
+    arrayWords.forEach( el => {
+      this.totalItem = new Component(listContainer, "li", ["total-item", "total-correct__item"]);
+      this.totalItemBtn = new UIButton(this.totalItem.element, ["total-item__btn"], "");
+      this.totalItemImg = new Component(this.totalItemBtn.element, "img", ["total-item__img"]);
+      this.totalItemImg.element.setAttribute("src", "../../../../public/volume.svg");
+      this.totalItemImg.element.setAttribute("alt", "volume");
+      this.totalItemEnWord = new Component(this.totalItem.element, "span", ["total-item__word", "total-item__en"], el.word);
+      this.totalItemHyphen = new Component(this.totalItem.element, "span", [], "-");
+      this.totalItemRuWord = new Component(this.totalItem.element, "span", ["total-item__word"], el.wordTranslate);
+    });
+  };
+}
